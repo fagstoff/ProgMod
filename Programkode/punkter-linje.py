94a2c5ceccfcd868405e2a8c2624955b84c078a7
Tom Jarle Christiansen <fuzzbin@gmail.com>
2017-07-07 23:40:53 +0200

    Ny fagtekst
    Variabler: Ny fagtekst og eksempler (Uferdig)
    Tre-punker: Ny fagtekst/eksempel som passer godt til R1/Vektorer
    (Uferdig)

--- Programkode/punkter-linje.py
+++ Programkode/punkter-linje.py
@@ -35,10 +35,10 @@ def main():
     else:
         print("Punktene {}, {} og {} ligger ikke pa en rett linje".format(P1, P2, P3))
         # Skriver ut punktene i et koordinatsystem
-        plt.plot([P1[0], P2[0], P3[0]], [P1[1], P2[1], P3[1]], 'bo')
-        plt.ylabel('Y')
-        plt.xlabel('X')
-        plt.show()
+    plt.plot([P1[0], P2[0], P3[0]], [P1[1], P2[1], P3[1]], 'bo')
+    plt.ylabel('Y')
+    plt.xlabel('X')
+    plt.show()
 
 if __name__ == '__main__':
     # Programmet starter her. Vi kaller på funksjonen main().
