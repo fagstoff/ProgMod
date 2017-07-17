dffd9566aaedf05c8bb5fd0e65c95179f745d771
Tom Jarle Christiansen <fuzzbin@gmail.com>
2017-07-17 14:49:23 +0200

    Fikset plotting

--- Programkode/rovdyr.py
+++ Programkode/rovdyr.py
@@ -9,22 +9,26 @@ b = 0.1 #beta
 c = 1.5 #delta
 d = 0.75 #gamma
 
-def dX_dt(X, t):
-    """ Return the growth rate of fox and rabbit populations. X[0]=x og X[1]=y"""
-    return ([ a*X[0] -   b*X[0]*X[1] ,
-                  -c*X[1] + d*b*X[0]*X[1] ])
+# Pack up the parameters and initial conditions:
+p = [a, b, c, d]
+w0 = [5, 15]
 
-t = np.linspace(0,2,2) # x-verdier (Tidsaksen)
-X0 = [10, 5]  # initialbetingelse v0 = 0 m/s
-X = odeint(dX_dt, X0, t, full_output=True) # Løser difflikningen
+def dX_dt(X, t, p):
+    """ Return the growth rate of fox and rabbit populations. X[0]=x og X[1]=y"""
+    a, b, c, d = p
+    return ([ a*X[0] -   b*X[0]*X[1], -c*X[1] + d*b*X[0]*X[1] ])
+
+t = np.linspace(0,10,100) # x-verdier (Tidsaksen)
+# X0 = [10, 5]  # initialbetingelse v0 = 0 m/s
+X = odeint(dX_dt, w0, t, args=(p,)) # Løser difflikningen
 X = np.array(X).flatten() # Formaterer tallene i en array
 
-print(X)
+# print(X[2])
 
-#
-#plt.grid(True)
-#plt.title("Fart som funksjon av tid", fontsize=16)
-#plt.xlabel('Tid [s]',fontsize=12)
-#plt.ylabel('Fart [m/s]',fontsize=12)
-#plt.plot(t,X)
-#plt.show()
+
+plt.grid(True)
+plt.title("Fart som funksjon av tid", fontsize=16)
+plt.xlabel('Tid [s]',fontsize=12)
+plt.ylabel('Fart [m/s]',fontsize=12)
+plt.plot(X)
+plt.show()
