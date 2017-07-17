97c80d09a768f0c51917c16ff6c135e3d9864602
Tom Jarle Christiansen <fuzzbin@gmail.com>
2017-07-17 18:13:37 +0200

    Fiksing av lints i kode.

--- Programkode/rovdyr.py
+++ Programkode/rovdyr.py
@@ -1,33 +1,37 @@
+""" Rovdyr - Lotka/Volterra - Rovdyr/Byttedyr-modell """
+#
+# Lisens: Creative Commons BY-SA fuzzbin (Tom Jarle Christiansen) og bitjungle (Rune Mathisen) 2017
+#
+# Program simulerer hvordan populasjonen av rovdyr/byttedyr påvirkes over tid
+
 import numpy as np
 # import scipy as sp
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
  # Definition of parameters
-a = 1. #alfa
-b = 0.1 #beta
-c = 1.5 #delta
-d = 0.75 #gamma
-
-w0 = [5, 15] # initialbetingelser
+A = 1. #alfa
+B = 0.1 #beta
+C = 1.5 #delta
+D = 0.75 #gamma
 
 # Definerer differensiallikningen
-def dX_dt(X, t, a, b, c, d):
+def d_pop(X, t_0, a_0, b_0, c_0, d_0):
     """ Returnerer antall rev og kanin. X[0]=x og X[1]=y"""
-    return ([ a*X[0] -   b*X[0]*X[1], -c*X[1] + d*b*X[0]*X[1] ])
+    return ([a_0*X_0[0] -   b_0*X[0]*X[1], -c_0*X[1] + d_0*b_0*X[0]*X[1]])
 
 
 # Løser differensiallikningen
-t = np.linspace(0,15,200) # x-verdier (Tidsaksen)
-w0 = [5, 15] # initialbetingelser 5 kaniner og 15 rev
-X = odeint(dX_dt, w0, t, args=(a, b, c, d)) # Løser difflikningen
+T = np.linspace(0, 15, 200) # x-verdier (Tidsaksen)
+P0 = [5, 15] # initialbetingelser 5 kaniner og 15 rev
+P = odeint(d_pop, P0, T, args=(A, B, C, D)) # Løser difflikningen
 
 # Utskrift
 plt.grid(True)
 plt.title("Populasjoner som funksjon av tid", fontsize=16)
-plt.xlabel('Tid',fontsize=12)
-plt.ylabel('Populasjon [antall individer]',fontsize=12)
-plt.plot(t, X[:, 0], 'b', label='Rev')
-plt.plot(t, X[:, 1], 'g', label='Kanin')
+plt.xlabel('Tid', fontsize=12)
+plt.ylabel('Populasjon [antall individer]', fontsize=12)
+plt.plot(T, P[:, 0], 'r', label='Rev')
+plt.plot(T, P[:, 1], 'b', label='Kanin')
 plt.legend(loc='best')
 plt.show()
