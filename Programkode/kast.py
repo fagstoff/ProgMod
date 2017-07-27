7b52b0f24936f71c0fc639b933f3cbb05f4d79d6
Tom Jarle Christiansen <fuzzbin@gmail.com>
2017-07-27 21:44:49 +0200

    Fikset lint

--- Programkode/kast.py
+++ Programkode/kast.py
@@ -3,31 +3,32 @@
 # Lisens: Creative Commons BY-SA fuzzbin (Tom Jarle Christiansen) og bitjungle (Rune Mathisen) 2017
 #
 # Program simulerer posisjonen til et objekt som kastes skrått
+# Vi ser bort fra alle andre krefter enn gravitasjonen i dette eksempelet.
 
+# Henter inn biblioteker som skal brukes
 import numpy as np
-# import scipy as sp
 import matplotlib.pyplot as plt
 
 # Definerer initialbetingelser
-v_0x = 20
-v_0y = 25
-g = 9.81
-tid = np.linspace(0.0, 5.0, num=50) # 50 tidsmerker mellom 0.0 og 5.0
+VX0 = 20
+VY0 = 25
+GRAV = 9.81
+TID = np.linspace(0.0, 5.0, num=50) # 50 tidsmerker mellom 0.0 og 5.0
 
 # Definerer modeller for x- og y-posisjon
-def y_pos(t):
-    return v_0y*t - 0.5*g*t**2
+def y_pos(tid):
+    """ Beregner y-posisjonen til objektet """
+    return VY0*tid - 0.5*GRAV*tid**2
 
-def x_pos(t):
-    return v_0x*t
-
-
+def x_pos(tid):
+    """ Beregner x-posisjonen til objektet """
+    return VX0*tid
 
 # Utskrift
 plt.grid(True)
 plt.title("Posisjon som funksjon av tid", fontsize=16)
 plt.xlabel('Lengde [meter]', fontsize=12)
 plt.ylabel('Høyde [meter]', fontsize=12)
-plt.plot(x_pos(tid), y_pos(tid), 'r', label='Ballens posisjon')
+plt.plot(x_pos(TID), y_pos(TID), 'r', label='Ballens posisjon')
 plt.legend(loc='best')
 plt.show()
