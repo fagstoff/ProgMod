af903bfc3dcf26d39c27d36737cda6297bfa72a3
Tom Jarle Christiansen <fuzzbin@gmail.com>
2017-09-21 11:05:52 +0200

    Nye variabelnavn

--- Programkode/nullpunkter.py
+++ Programkode/nullpunkter.py
@@ -7,20 +7,20 @@
 
 import math # Henter inn matematikkbiblioteket
 
-print('Tast inn A, B og C')
-ROTVERDI = -1 # Setter en startverdi for å få whileløkka til å kjøre første gang.
+print('Tast inn a, b og c')
+rotverdi = -1 # Setter en startverdi for å få whileløkka til å kjøre første gang.
 
-while ROTVERDI < 0: # Løkke som leser inn verdier så lenge ROTVERDI er mindre enn null
-    A = int(input('Tast inn A: '))
-    B = int(input('Tast inn B: '))
-    C = int(input('Tast inn C: '))
-    ROTVERDI = math.pow(B, 2) - (4*A*C)
-    if ROTVERDI < 0:
+while rotverdi < 0: # Løkke som leser inn verdier så lenge ROTVERDI er mindre enn null
+    a = int(input('Tast inn a: '))
+    b = int(input('Tast inn b: '))
+    c = int(input('Tast inn c: '))
+    rotverdi = math.pow(b, 2) - (4*a*c)
+    if rotverdi < 0:
         print('Ingen losning. Prov igjen') # Feilmelding
 
 # Regner ut ABC-formel
-X1 = -B + math.sqrt(ROTVERDI) / (2 * A)
-X2 = -B - math.sqrt(ROTVERDI) / (2 * A)
+x1 = -b + math.sqrt(rotverdi) / (2 * a)
+x2 = -b - math.sqrt(rotverdi) / (2 * a)
 
-print('Nullpunktene til funksjonen er: X1={} og X2={}'.format(X1, X2))
+print('Nullpunktene til funksjonen er: X1={} og X2={}'.format(x1, x2))
 print('Ha en fin dag!')
