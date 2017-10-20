196eca84c4b3219cd2a91fad848ea3a138d68cd6
bitjungle <devel@bitjungle.com>
2017-10-20 21:18:29 +0200

    the field is now an object

--- Programkode/fotballpong_v4.py
+++ Programkode/fotballpong_v4.py
@@ -10,6 +10,22 @@ TODO versjon 5:
 import sys
 import pygame
 
+class Field():
+    '''The football field'''
+    def __init__(self, **kwargs):
+        super(Field, self).__init__()
+        self._vars = kwargs
+        self.image = pygame.image.load(self.get_variable('image'))
+        self.rect = self.image.get_rect()
+
+    def get_variable(self, key):
+        '''Get a variable value, returns None if the variable is not set'''
+        return self._vars.get(key, None)
+
+    def set_variable(self, key, val):
+        '''Set a variable value'''
+        self._vars[key] = val
+
 class MovingImage():
     '''A moving image object'''
     def __init__(self, **kwargs):
@@ -41,7 +57,7 @@ class MovingImage():
     def set_variable(self, key, val):
         '''Set a variable value'''
         self._vars[key] = val
-    
+
 class Player(MovingImage):
     '''Football player object'''
     def __init__(self, **kwargs):
@@ -76,21 +92,19 @@ def main():
     GOLD = (255,215,0)
 
     # Loading images
-    field = pygame.image.load('fotballpong_bane.png')
+    field = Field(image='fotballpong_bane.png')
     ball = Ball(image="fotballpong_ball.png", offset=[5, 5])
     p1 = Player(image="fotballpong_spiller_v.png")
     p2 = Player(image="fotballpong_spiller_h.png")
-
-    fieldrect = field.get_rect()
 
     goal1 = pygame.Surface((25,100))
     goal2 = pygame.Surface((25,100))
     goal1rect = goal1.get_rect()
     goal2rect = goal2.get_rect()
-    goal1rect.top = (fieldrect.height // 2) - (goal1rect.height // 2)
-    goal2rect.top = (fieldrect.height // 2) - (goal2rect.height // 2)
+    goal1rect.top = (field.rect.height // 2) - (goal1rect.height // 2)
+    goal2rect.top = (field.rect.height // 2) - (goal2rect.height // 2)
     goal1rect.left = 25
-    goal2rect.left = fieldrect.width - 50
+    goal2rect.left = field.rect.width - 50
     goal1.fill(LIGHT_GRAY)
     goal2.fill(LIGHT_GRAY)
 
@@ -98,7 +112,7 @@ def main():
     FIELD_PADDING_LEFTRIGHT = 50
     FIELD_PADDING_TOPBOTTOM = 25
 
-    screen = pygame.display.set_mode(fieldrect.size)
+    screen = pygame.display.set_mode(field.rect.size)
     caption = pygame.display.set_caption("Fotballpong v4")
     clock = pygame.time.Clock()
     FPS = 50 # Limit frames per second
@@ -106,18 +120,18 @@ def main():
     # Preparing the scoreboard
     font = pygame.font.Font("SourceCodePro-Regular.ttf", 24)
     TEXT_POS_TOP = 20
-    PLAYER1_SCORE_POS_LEFT = fieldrect.width // 8
-    PLAYER2_SCORE_POS_LEFT = (fieldrect.width // 2) + (fieldrect.width // 8)
-    GOAL_POS_LEFT = (fieldrect.width // 2)
+    PLAYER1_SCORE_POS_LEFT = field.rect.width // 8
+    PLAYER2_SCORE_POS_LEFT = (field.rect.width // 2) + (field.rect.width // 8)
+    GOAL_POS_LEFT = (field.rect.width // 2)
 
     # Set initial player positions
-    p1.rect.top = (fieldrect.height // 2) - (p1.rect.height // 2)
-    p2.rect.top = (fieldrect.height // 2) - (p2.rect.height // 2)
-    p1.rect.left = (fieldrect.width // 2) - (fieldrect.width // 4) - p1.rect.width
-    p2.rect.left = (fieldrect.width // 2) + (fieldrect.width // 4)
+    p1.rect.top = (field.rect.height // 2) - (p1.rect.height // 2)
+    p2.rect.top = (field.rect.height // 2) - (p2.rect.height // 2)
+    p1.rect.left = (field.rect.width // 2) - (field.rect.width // 4) - p1.rect.width
+    p2.rect.left = (field.rect.width // 2) + (field.rect.width // 4)
 
     # Set initial ball position
-    center_rect_on_rect(ball.rect, fieldrect)
+    center_rect_on_rect(ball.rect, field.rect)
 
     goal_scored = False
 
@@ -146,29 +160,29 @@ def main():
         elif ball.colliderect(goal1rect):
             p2.increment_score()
             ball.flip_horiz()
-            center_rect_on_rect(ball.rect, fieldrect)
+            center_rect_on_rect(ball.rect, field.rect)
             goal_scored = True
         elif ball.rect.colliderect(goal2rect):
             p1.increment_score()
             ball.flip_horiz()
-            center_rect_on_rect(ball.rect, fieldrect)
+            center_rect_on_rect(ball.rect, field.rect)
             goal_scored = True
-        elif ball.rect.left < FIELD_PADDING_LEFTRIGHT or ball.rect.right > fieldrect.width - FIELD_PADDING_LEFTRIGHT: 
+        elif ball.rect.left < FIELD_PADDING_LEFTRIGHT or ball.rect.right > field.rect.width - FIELD_PADDING_LEFTRIGHT: 
             ball.flip_horiz()
-        elif ball.rect.top - FIELD_PADDING_TOPBOTTOM < 0 or ball.rect.bottom > fieldrect.height - FIELD_PADDING_TOPBOTTOM:
+        elif ball.rect.top - FIELD_PADDING_TOPBOTTOM < 0 or ball.rect.bottom > field.rect.height - FIELD_PADDING_TOPBOTTOM:
             ball.flip_vert()
         else:
             pass # Do nothing
         
         # Move player 1, check if he collides with an edge
         p1.move()
-        if p1.rect.top < 0 or p1.rect.bottom > fieldrect.height:
+        if p1.rect.top < 0 or p1.rect.bottom > field.rect.height:
             p1.flip_vert()
             p1.move()
 
         # Move player 2, check if he collides with an edge
         p2.move()
-        if p2.rect.top < 0 or p2.rect.bottom > fieldrect.height:
+        if p2.rect.top < 0 or p2.rect.bottom > field.rect.height:
             p2.flip_vert()
             p2.move()
 
@@ -179,7 +193,7 @@ def main():
             pygame.display.flip()
             pygame.time.wait(1000)
             goal_scored = False # resetting
-        screen.blit(field, fieldrect)
+        screen.blit(field.image, field.rect)
         screen.blit(player1_score_txt,(PLAYER1_SCORE_POS_LEFT, TEXT_POS_TOP))
         screen.blit(player2_score_txt,(PLAYER2_SCORE_POS_LEFT, TEXT_POS_TOP))
         screen.blit(p1.image, p1.rect)
