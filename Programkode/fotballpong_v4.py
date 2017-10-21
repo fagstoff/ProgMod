f3fb2b051547d7ec5a16c30b6aec2e3c10c9331b
bitjungle <devel@bitjungle.com>
2017-10-21 07:16:35 +0200

    class defs for all game objects

--- Programkode/fotballpong_v4.py
+++ Programkode/fotballpong_v4.py
@@ -1,21 +1,25 @@
 '''
-Fotballpong v3
+Fotballpong v4
 Lisens: Creative Commons BY-SA bitjungle
-TODO versjon 4:
-* Objektorientere
-* Lydeffekter
 TODO versjon 5:
+* Lydeffekter
+TODO versjon 6:
 * Player 2 AI
 '''
 import sys
 import pygame
 
-class Field():
-    '''The football field'''
+# Color definitions
+PALE_GREEN = (152,251,152)
+LIGHT_GRAY = (211,211,211)
+GOLD = (255,215,0)
+
+class GameObject(pygame.sprite.Sprite):
+    '''Base class for all game objects'''
     def __init__(self, **kwargs):
-        super(Field, self).__init__()
+        super(GameObject, self).__init__()
         self._vars = kwargs
-        self.image = pygame.image.load(self.get_variable('image'))
+        self.image = pygame.Surface((0,0))
         self.rect = self.image.get_rect()
 
     def get_variable(self, key):
@@ -26,11 +30,51 @@ class Field():
         '''Set a variable value'''
         self._vars[key] = val
 
-class MovingImage():
+    def set_top(self, pos):
+        self.rect.top = pos 
+
+    def set_left(self, pos):
+        self.rect.left = pos 
+    
+    def center_on_rect(self, target_rect):
+        '''Center the object in the middle of a target rect'''
+        self.rect.top = (target_rect.height // 2) - (self.rect.height // 2)
+        self.rect.left = (target_rect.width // 2) - (self.rect.width // 2)
+
+
+class Field(GameObject):
+    '''The football field'''
+    def __init__(self, **kwargs):
+        super(Field, self).__init__(**kwargs)
+        self.image = pygame.image.load(self.get_variable('image'))
+        self.rect = self.image.get_rect()
+
+class Goal(GameObject):
+    '''Football goal object'''
+    def __init__(self, **kwargs):
+        super(Goal, self).__init__(**kwargs)
+
+        if self.get_variable('width') is None:
+            self.set_variable('width', 25)
+        if self.get_variable('height') is None:
+            self.set_variable('height', 100)
+
+        self.image = pygame.Surface((self._vars['width'],self._vars['height']))
+        self.rect = self.image.get_rect()
+        
+        if self.get_variable('top') is None:
+            self.set_top(0)
+        if self.get_variable('left') is None:
+            self.set_left(0)
+        
+        if self.get_variable('fill') is None:
+            self.image.fill(LIGHT_GRAY)
+
+
+class MovingImage(GameObject):
     '''A moving image object'''
     def __init__(self, **kwargs):
-        super(MovingImage, self).__init__()
-        self._vars = kwargs
+        super(MovingImage, self).__init__(**kwargs)
         self.image = pygame.image.load(self.get_variable('image'))
         self.rect = self.image.get_rect()
         if self.get_variable('offset') is None:
@@ -49,14 +93,6 @@ class MovingImage():
     def flip_vert(self):
         '''Flip the y value in the offset [x, y] pair'''
         self._vars['offset'][1] = -self._vars['offset'][1]
-
-    def get_variable(self, key):
-        '''Get a variable value, returns None if the variable is not set'''
-        return self._vars.get(key, None)
-
-    def set_variable(self, key, val):
-        '''Set a variable value'''
-        self._vars[key] = val
 
 class Player(MovingImage):
     '''Football player object'''
@@ -77,37 +113,23 @@ class Ball(MovingImage):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-def center_rect_on_rect(rect, target_rect):
-    '''Center a rect in the middle of a target rect'''
-    rect.top = (target_rect.height // 2) - (rect.height // 2)
-    rect.left = (target_rect.width // 2) - (rect.width // 2)
-
 def main():
     '''Game main loop'''
     pygame.init()
 
-    # Color definitions
-    PALE_GREEN = (152,251,152)
-    LIGHT_GRAY = (211,211,211)
-    GOLD = (255,215,0)
-
-    # Loading images
+    # Creating game objects
     field = Field(image='fotballpong_bane.png')
     ball = Ball(image="fotballpong_ball.png", offset=[5, 5])
     p1 = Player(image="fotballpong_spiller_v.png")
     p2 = Player(image="fotballpong_spiller_h.png")
 
-    goal1 = pygame.Surface((25,100))
-    goal2 = pygame.Surface((25,100))
-    goal1rect = goal1.get_rect()
-    goal2rect = goal2.get_rect()
-    goal1rect.top = (field.rect.height // 2) - (goal1rect.height // 2)
-    goal2rect.top = (field.rect.height // 2) - (goal2rect.height // 2)
-    goal1rect.left = 25
-    goal2rect.left = field.rect.width - 50
-    goal1.fill(LIGHT_GRAY)
-    goal2.fill(LIGHT_GRAY)
-
+    g1 = Goal()
+    g1.set_top((field.rect.height // 2) - (g1.rect.height // 2))
+    g1.set_left(25)
+    g2 = Goal()
+    g2.set_top((field.rect.height // 2) - (g1.rect.height // 2))
+    g2.set_left(field.rect.width - 50)
+    
      # Ball will bounce x pixels from the edge of the field
     FIELD_PADDING_LEFTRIGHT = 50
     FIELD_PADDING_TOPBOTTOM = 25
@@ -122,18 +144,19 @@ def main():
     TEXT_POS_TOP = 20
     PLAYER1_SCORE_POS_LEFT = field.rect.width // 8
     PLAYER2_SCORE_POS_LEFT = (field.rect.width // 2) + (field.rect.width // 8)
-    GOAL_POS_LEFT = (field.rect.width // 2)
+    GOAL_TXT_POS_LEFT = (field.rect.width // 2)
 
     # Set initial player positions
-    p1.rect.top = (field.rect.height // 2) - (p1.rect.height // 2)
-    p2.rect.top = (field.rect.height // 2) - (p2.rect.height // 2)
-    p1.rect.left = (field.rect.width // 2) - (field.rect.width // 4) - p1.rect.width
-    p2.rect.left = (field.rect.width // 2) + (field.rect.width // 4)
+    p1.set_top((field.rect.height // 2) - (p1.rect.height // 2))
+    p2.set_top((field.rect.height // 2) - (p2.rect.height // 2))
+    p1.set_left((field.rect.width // 2) - (field.rect.width // 4) - p1.rect.width)
+    p2.set_left((field.rect.width // 2) + (field.rect.width // 4))
 
     # Set initial ball position
-    center_rect_on_rect(ball.rect, field.rect)
+    ball.center_on_rect(field.rect)
 
     goal_scored = False
+    goal_text = font.render("GOAL!", True, GOLD)
 
 
     while True:
@@ -145,9 +168,9 @@ def main():
             if key[pygame.K_o]: p2.set_offset(-4)
             if key[pygame.K_l]: p2.set_offset(4)
 
+        # Creating/updating the score board
         player1_score_txt = font.render("Player 1: {}".format(p1.get_variable('score')), True, PALE_GREEN)
         player2_score_txt = font.render("Player 2: {}".format(p2.get_variable('score')), True, PALE_GREEN)
-        goal_text = font.render("GOAL!", True, GOLD)
 
         # Moving the ball
         ball.move()
@@ -157,15 +180,15 @@ def main():
             ball.flip_horiz()
         elif ball.colliderect(p2.rect):
             ball.flip_horiz()
-        elif ball.colliderect(goal1rect):
+        elif ball.colliderect(g1.rect):
             p2.increment_score()
             ball.flip_horiz()
-            center_rect_on_rect(ball.rect, field.rect)
+            ball.center_on_rect(field.rect)
             goal_scored = True
-        elif ball.rect.colliderect(goal2rect):
+        elif ball.rect.colliderect(g2.rect):
             p1.increment_score()
             ball.flip_horiz()
-            center_rect_on_rect(ball.rect, field.rect)
+            ball.center_on_rect(field.rect)
             goal_scored = True
         elif ball.rect.left < FIELD_PADDING_LEFTRIGHT or ball.rect.right > field.rect.width - FIELD_PADDING_LEFTRIGHT: 
             ball.flip_horiz()
@@ -189,7 +212,7 @@ def main():
         # Print images to screen
         if goal_scored == True:
             #BUG teksten vises ikke
-            screen.blit(goal_text,(GOAL_POS_LEFT-28, TEXT_POS_TOP+150))
+            screen.blit(goal_text,(GOAL_TXT_POS_LEFT-28, TEXT_POS_TOP+150))
             pygame.display.flip()
             pygame.time.wait(1000)
             goal_scored = False # resetting
@@ -199,8 +222,8 @@ def main():
         screen.blit(p1.image, p1.rect)
         screen.blit(p2.image, p2.rect)
         screen.blit(ball.image, ball.rect)
-        screen.blit(goal1, goal1rect)
-        screen.blit(goal2, goal2rect)
+        screen.blit(g1.image, g1.rect)
+        screen.blit(g2.image, g2.rect)
         pygame.display.flip()
 
         clock.tick(FPS)
