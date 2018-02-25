fe09d93cc076dcc683c718d21fc15fcfd070be31
bitjungle <devel@bitjungle.com>
2018-02-25 20:18:58 +0100

    Rydder og finpusser

--- Programkode/Diverse/anim_kast.py
+++ Programkode/Diverse/anim_kast.py
@@ -1,8 +1,9 @@
-""" Animasjon av skrått kast """
+""" Animasjon av vertikalt og skrått kast """
 # Lisens: Creative Commons BY-SA fuzzbin (Tom Jarle Christiansen) og bitjungle (Rune Mathisen) 
 #
-# Animasjon av et skrått kast. Viser en ball som beveger seg friksjonsfritt
-# med en fart på 10 m/s i x-retning og en startfart på 20 m/s i y-retning. 
+# Animasjon av et vertikalt eller skrått kast. 
+# Viser en ball som beveger seg friksjonsfritt.
+# Sett v0x til 0 for et vertikalt kast.
 # Ett piksel telles som 0,5 meter i denne animasjonen. 
 import sys
 import pygame
@@ -12,14 +13,17 @@ import numpy as np
 def fart(v0, a, tid):
     return v0 + a * tid
 
+def ball_til_startpunkt():
+    ballrect.top = fieldrect.height - ballrect.height
+    ballrect.left = 10
+
 
 # Definerer initialbetingelsene til modellen
-s0 = 0    # m
-v0x = 15  # m/s
-v0y = 25  # m/s
-a = -9.81 # m/s^2
+v0x = 15  # Fart i x-retning (m/s). Sett til 0 for vertikalt kast
+v0y = 25  # Fart u y-retning (m/s).
+a = -9.81 # Akselerasjon nedover (m/s^2)
 
-teller = 0 # index
+teller = 0 # Holder styr på hvor langt i simuleringen vi har kommet.
 
 # Liste med x-verdier - tiden
 tx = np.arange(0, 6, 0.05)
@@ -27,8 +31,7 @@ vy = [] # tom liste som skal inneholde fart som funksjon av tid
 
 # Genererer v-verdier verdier for alle tidspunkt t
 for x in tx:
-    vy.append(-fart(v0y, a, x)) # Studer denne! Legger til ny verdi i lista (append).
-                        #Minus grunnet koordinatsystemet.
+    vy.append(-fart(v0y, a, x))
 
 # Starter pygame
 pygame.init()
@@ -39,27 +42,27 @@ ball = pygame.image.load('anim_fotball.png')
 fieldrect = field.get_rect()
 ballrect = ball.get_rect()
 
-# Setter vindusstørrelsen til det samme som størrelsen på bildet 
+# Setter vindusstørrelsen til det samme som størrelsen på bildet av banen.
 screen = pygame.display.set_mode(fieldrect.size)
 
-# Plasserer ballen der vi vil at den skal starte
-ballrect.top = fieldrect.height - ballrect.height
-ballrect.left = 10
+# Plasserer ballen der vi vil at den skal starte.
+ball_til_startpunkt()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+        if event.type == pygame.QUIT: sys.exit()
 
-    try: # Flytter ballen og øker teller
+    try: # Flytter ballen og øker telleren.
+        # Deler på 2 fordi ett piksel tilsvarer en halv meter.
         ballrect = ballrect.move([v0x // 2, vy[teller] // 2])
         teller += 1
-    except IndexError: # Vi har kommet til enden av lista
-        # Nullstiller teller
-        teller = 0
-        # Flytter ball tilbake til startpunkt
-        ballrect.top = fieldrect.height - ballrect.height
-        ballrect.left = 10
+    except IndexError: # Vi har kommet til enden av lista.
+        teller = 0 # Nullstiller telleren.
+        ball_til_startpunkt() # Flytter ballen tilbake til startpunkt.
+
+    if ballrect.bottom > fieldrect.height: # Sjekker om ballen treffer nedkanten av banen.
+        teller = 0 # Nullstiller telleren.
+        ball_til_startpunkt() # Flytter ballen tilbake til startpunkt.
 
     # Skriver ut bakgrunnsbilde og ballen i sin nye posisjon.
     screen.blit(field, fieldrect)
